46bea43813abdfd8383e3f8072b957040bf9287d
UsmanMern <mu1363131@gmail.com>
2023-12-22 09:03:53 +0500

    Your commit message here

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 // const express = require("express");
 // const dotenv = require("dotenv");
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config()
-const app = express()
 
-app.get("/", (req, res) => {
-    res.json("Server is running oo yeah")
-})
+// import express from "express";
+// import dotenv from "dotenv";
+// dotenv.config()
+// const app = express()
+
+// app.get("/", (req, res) => {
+//     res.json("Server is running oo yeah")
+// })
 
 
 // const port = process.env.PORT || 8000
 
 
-app.listen(port, () => {
-    console.log("app is running on PORT", port);
-})
+// app.listen(port, () => {
+//     console.log("app is running on PORT", port);
+// })
+
+
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+const FILE_PATH = "./data.json"
+
+const DATE = moment().format()
+
+const date = {
+    date: DATE
+}
+
+jsonfile.writeFile(FILE_PATH, date)
