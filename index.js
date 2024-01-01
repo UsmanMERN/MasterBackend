237219d37b06c457a52295e10e4632f277fe5605
UsmanMERN <mu1363131@gmail.com>
2024-01-01 22:41:43 +0500

    modeling

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 
 
 const port = process.env.PORT || 8000
-// const port = process.env.PORT || 8000
+// const port = process.env.PORT || 8000 yawo
 
 
 app.listen(port, () => {
